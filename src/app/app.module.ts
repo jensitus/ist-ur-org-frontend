34e8b86661e920f86799c5e6acbf0dd99001f317
ykoitzsch <y.koitzsch@gmx.at>
2019-04-04 23:16:04 +0200

    topbar hinzugefügt mit menüpunkten und searchbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes} from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import {ProgressSpinnerModule} from 'primeng/progressspinner';
-import {ButtonModule} from 'primeng/button';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { ButtonModule } from 'primeng/button';
+import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { InputTextModule } from 'primeng/inputtext';
 
 const app_routes: Routes = [
   {path: 'home', component: HomeComponent},
@@ -15,13 +18,16 @@ const app_routes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(app_routes, {enableTracing: true}),
     ProgressSpinnerModule,
-    ButtonModule
+    ButtonModule,
+    AppRoutingModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
