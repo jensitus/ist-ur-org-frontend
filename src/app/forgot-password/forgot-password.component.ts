e4eeda712d2e771e4f25ca8bcd564a6153b77370
Jens <jens@ist-ur.org>
2019-04-27 13:58:36 +0200

    reset your password

---
+++ src/app/forgot-password/forgot-password.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {UserService} from '../services/user.service';
+import {Router} from '@angular/router';
+import {first} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.css']
+})
+export class ForgotPasswordComponent implements OnInit {
+
+  forgotForm: FormGroup;
+  loading = false;
+  submitted = false;
+  data: any;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private userService: UserService
+  ) { }
+
+  ngOnInit() {
+    this.forgotForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  get f() {
+    return this.forgotForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.forgotForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.userService.forgotPassword(this.forgotForm.value).subscribe(data => {
+        console.log(data);
+        this.data = data;
+        // this.alertService.success(this.data.message, true);
+        this.router.navigate(['/login']);
+      }, error => {
+        console.log('error', error);
+        // this.alertService.error(error);
+      }
+    );
+  }
+
+}
