2b23dbc7edb1421942cd474dc8d4a8836cd667e2
jensitus <jens@service-b.org>
2021-04-27 22:34:35 +0200

    no spec needed

--- src/app/common/pipes/mention-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Url2linkPipe } from './url2link.pipe';
-
-describe('MentionPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new Url2linkPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
